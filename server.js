6f86033a92f0b6668180d789428492d34d2f90fa
Harold Camas <haroldcamas99@gmail.com>
2024-12-13 04:53:19 -0600

    Endpoint deletePost

--- server.js
+++ server.js
@@ -113,7 +113,6 @@ app.post('/login', async (req, res) => {
 
 
 //logout 
-
 app.post('/logout', async (req, res) => {
     
     try {
@@ -137,13 +136,13 @@ app.post('/createPost', async (req,res) => {
     const {title, content, user} = req.body;
     
     try{
-        if (!title || !content || !user) {
-            return res.status(400).json({ error: 'Se requiere llenar toda la informacion' });
-        }
-
         const collection = db.collection('posts');
-        const result = await collection.insertOne({ title, content });
-        res.status(200).send({
+        const result = await collection.insertOne({ 
+            title: title, 
+            content: content,
+            user: user
+         });
+        res.status(201).send({
             title: title,
             content: content,
             user: user,
@@ -161,7 +160,7 @@ app.post('/createPost', async (req,res) => {
 app.get("/listPost", async(req,res) =>{
     try{
         const collection = db.collection('posts');
-        const lista = await collection.find().toArray();
+        const lista = await collection.find({}).toArray();
         res.status(200).send({
             list: lista,
             message: "Lista de Posts obtenida"
@@ -177,7 +176,7 @@ app.get("/listPost", async(req,res) =>{
 //editar post
 app.put("/editPost/:id", async (req,res) => {
     const id = req.params.id;
-    const {newTitle, newContent} = req.body;
+    const {newTitle, newContent, user} = req.body;
 
     try {
         const collection = db.collection('posts');
@@ -185,18 +184,11 @@ app.put("/editPost/:id", async (req,res) => {
 
         const result = await collection.updateOne(
             { id: objID },
-            { $set: {title: newTitle, content: newContent}  });
+            { $set: {title: newTitle, content: newContent, user: user}  });
 
-        if (result.matchedCount === 0) {
-            res.status(404).send({ 
-                message: 'Post not found' 
-            });
-        }
-        else{
             res.status(200).send({
                 message: "Se modifico el post con exito!"
             })
-        }
 
     }catch(e){
         res.status(401).send({
@@ -206,3 +198,29 @@ app.put("/editPost/:id", async (req,res) => {
 });
 
 //eliminar post
+app.delete("/deletePost/:id", async (req,res) => {
+    const id = req.params.id;
+    try{
+        const collection = db.collection('posts');
+        const objID = new ObjectId(id);
+        const result = await collection.deleteOne(
+            {id: objID}
+        );
+
+        if (result.deletedCount > 0) {
+            res.status(200).send({
+                resultado: result,
+                mensaje: "Post eliminado con exito!",
+            });
+        } else {
+            res.status(404).send({
+                error: "Post not found",
+            });
+        }
+
+    }catch(e){
+        res.status(401).send({
+            error:e.message
+        });
+    }
+});
