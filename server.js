36b6e04db58e8c870c350d120966ab287ceb9e1a
Harold Camas <haroldcamas99@gmail.com>
2024-12-13 04:30:52 -0600

    Endpoint editPost

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const strict =  require('assert');
 const { initializeApp } = require('firebase/app');
 const session = require('express-session');
@@ -131,11 +131,13 @@ app.post('/logout', async (req, res) => {
 
 
 /*posts de mongo*/
+
+//crear posts
 app.post('/createPost', async (req,res) => {
-    const {title, content} = req.body;
+    const {title, content, user} = req.body;
     
     try{
-        if (!title || !content) {
+        if (!title || !content || !user) {
             return res.status(400).json({ error: 'Se requiere llenar toda la informacion' });
         }
 
@@ -144,6 +146,7 @@ app.post('/createPost', async (req,res) => {
         res.status(200).send({
             title: title,
             content: content,
+            user: user,
             message: "Post Creado exitosamente"
         })
 
@@ -154,12 +157,14 @@ app.post('/createPost', async (req,res) => {
     }
 });
 
+//lista de posts
 app.get("/listPost", async(req,res) =>{
     try{
         const collection = db.collection('posts');
         const lista = await collection.find().toArray();
         res.status(200).send({
-            message: "Post: \n" + lista
+            list: lista,
+            message: "Lista de Posts obtenida"
         })
 
     }catch(e){
@@ -169,6 +174,35 @@ app.get("/listPost", async(req,res) =>{
     }
 });
 
+//editar post
+app.put("/editPost/:id", async (req,res) => {
+    const id = req.params.id;
+    const {newTitle, newContent} = req.body;
 
+    try {
+        const collection = db.collection('posts');
+        const objID = new ObjectId(id);
 
+        const result = await collection.updateOne(
+            { id: objID },
+            { $set: {title: newTitle, content: newContent}  });
 
+        if (result.matchedCount === 0) {
+            res.status(404).send({ 
+                message: 'Post not found' 
+            });
+        }
+        else{
+            res.status(200).send({
+                message: "Se modifico el post con exito!"
+            })
+        }
+
+    }catch(e){
+        res.status(401).send({
+            error:e.message
+        });
+    }
+});
+
+//eliminar post
